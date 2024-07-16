e2e0afe353fdf255e0970d495aca784bcacbeb26
GustavoHolanda16 <holandsouza@gmail.com>
2024-07-16 19:57:08 -0300

    teste2

--- flask_back/app.py
+++ flask_back/app.py
@@ -4,10 +4,10 @@ app = Flask(__name__, static_url_path='/static')
 @app.route('/')
 def hello():
     return render_template('home.html')
-@app.route('/submit', methods=['POST'])
+@app.route('/submit', methods=['GET'])
 def submit():
     input_text = request.form['input_text']
-    return redirect(url_for('result', input_text=input_text))
+    return redirect(url_for('filmes', input_text=input_text))
 @app.route('/filmes')
 def filmes():
     input_text = request.args.get('input_text')
